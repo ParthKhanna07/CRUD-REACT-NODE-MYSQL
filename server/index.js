3af7a585fc88ab6ce0b979e35a628381fa39a787
Parth Khanna <46607748+ParthKhanna07@users.noreply.github.com>
2021-05-07 00:41:19 +0530

    Configuring MYSQL
    Joining MYSQL on localhost with Node

--- server/index.js
+++ server/index.js
@@ -1,9 +1,22 @@
 const express=require('express');
 const app=express();
+const mysql=require("mysql");
 
+const db=mysql.createPool({
+    host: "localhost",
+    user:'root',
+    password:"parth12345",
+    database:"CRUDDB"
+})
 app.get('/', (req,res) =>{
-    res.send("hello world");
+    const sqlInsert="INSERT INTO movie_reviews (movieName, movieReview) VALUES ('inception','good movie'); "
+    db.query(sqlInsert, (err,result) =>{
+        res.send("hello world");
+    })
+    
 });
+
+
 
 app.listen(3001, () => {
     console.log("port 3001");

1ca19047f441fcd70751e6d93a52164a5712cb78
Parth Khanna <46607748+ParthKhanna07@users.noreply.github.com>
2021-05-07 01:48:04 +0530

    Create and Retrieve
    Used Axios for requests to the servers, added data to MYSQL from frontend. Feth data from MYSQl.

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,47 @@
+import React ,{useState,useEffect} from "react"
 import logo from './logo.svg';
+import Axios from "axios"
 import './App.css';
 
 function App() {
+
+  const [movieName ,setMovieName]= useState('')
+  const [review ,setReview]= useState('')
+  const [movieReviewList,setMovieList]=useState([])
+
+  useEffect(() =>{
+    Axios.get("http://localhost:3001/api/get").then((response)=>{
+      console.log(response.data)
+      setMovieList(response.data)
+    })
+  },[])
+
+  const submitReview =() => {
+    Axios.post("http://localhost:3001/api/insert", {
+      movieName:movieName,
+      movieReview:review
+    }).then(()=>{
+      alert("success")
+    })
+  }
   return (
     <div className="App">
       <h1> CRUD APPLICATION </h1>
       <div className="form">
         <label>Movie Name</label>
-      <input type="text" name="movieName"></input>
+      <input type="text" name="movieName" onChange={(e)=>{
+        setMovieName(e.target.value)
+      }}></input>
       <label>Review</label>
-      <input type="text" name="review"></input>
-      <button>Submit</button>
+      <input type="text" name="review" onChange={(e)=>{
+        setReview(e.target.value)
+      }}></input>
+      <button onClick={submitReview}>Submit</button>
+
+      {movieReviewList.map((val)=>{
+        return <h1>Movie Name: {val.movieName} | Movie Review: {val.movieReview}</h1>
+
+      })}
       </div>
     </div>
   );
--- server/index.js
+++ server/index.js
@@ -1,4 +1,5 @@
 const express=require('express');
+const cors=require('cors')
 const app=express();
 const mysql=require("mysql");
 
@@ -8,13 +9,35 @@ const db=mysql.createPool({
     password:"parth12345",
     database:"CRUDDB"
 })
+app.use(express.json());
+app.use(express.urlencoded());
+app.use(cors())
+
+
 app.get('/', (req,res) =>{
-    const sqlInsert="INSERT INTO movie_reviews (movieName, movieReview) VALUES ('inception','good movie'); "
-    db.query(sqlInsert, (err,result) =>{
-        res.send("hello world");
-    })
+    
+    res.send("hello world");
     
 });
+
+app.get('/api/get', (req,res)=>{
+    const sqlSelect="SELECT * FROM movie_reviews  "
+    db.query(sqlSelect,  (err,result) =>{
+        res.send(result)
+    })
+})
+
+app.post("/api/insert", (req,res) =>{
+
+    const movieName=req.body.movieName
+     const movieReview=req.body.movieReview
+
+    const sqlInsert="INSERT INTO movie_reviews (movieName, movieReview) VALUES (?,?); "
+    db.query(sqlInsert, [movieName,movieReview], (err,result) =>{
+        console.log(result)
+    })
+})
+
 
 
 
